51b2f506a514fc3dc4c3f8fabe7b8c9e536590c2
Ana Laura Soethe Chagas <ana.soethe.chagas@wzl-iqs.rwth-aachen.de>
2025-10-21 16:10:00 +0200

    Changed structure for submodule

--- docker_build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/env bash
-docker build $1 -t ai-agent_spot_test:main -f ollama.Dockerfile .
--- entrypoint_scripts/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-
-# Source ROS workspaces
-source /opt/ros/humble/setup.bash
-
-source intall/setup.bash
-
-source /colcon_ws/install/setup.bash
